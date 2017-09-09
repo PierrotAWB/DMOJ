80aaa817e09c580837810b55eed7bbed640e60c9
PierrotAWB <andrewwang298@gmail.com>
2017-09-08 22:21:21 -0400

    Compete in BFS17

--- FastFactorial/main.cpp
+++ FastFactorial/main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-#include <math.h>
-#include <map>
 
-using namespace std;
+template<unsigned int n>
+struct factorial
+{
+    enum
+    {
+        value = n * factorial<n - 1>::value
+    };
+};
 
-map<int, long long> m = {{1, 1}};
-
-long long fact(long long k) {
-    if (m.find(k) == m.end()) {
-        m[k] = k * fact(k - 1);
-        return k * fact(k - 1);
-    }
-    return m[k];
-}
+template<>
+struct factorial<0>
+{
+    enum { value = 1 };
+};
 
 int main()
 {
-    int N;
-    cin >> N;
+    int N; 
+    std::cin >> N; 
 
-    for (int i = 0; i < N; i++){
-        long long k;
-        cin >> k;
-        cout << remainder(fact(k), pow(2, 32))<< "\n";
+    for (int i = 0; i < N; i++) { 
+        int K; 
+        std::cin >> K;
+        std::cout << factorial<K>::value << std::endl;  
     }
-    return 0;
 }
